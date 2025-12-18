47ad95db6cd4a7897f50d88e66833f0d277f3fa8
acbuie <aidancbuie@gmail.com>
2025-12-18 11:38:39 -0700

    refactor: Move to suggested layout from lazy.nvim

--- init.lua
+++ init.lua
@@ -1,1 +1,1 @@
-require("src.core")
+require("config")
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,21 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- jk
+keymap.set("i", "jk", "<esc>", { desc = "Exit Insert mode with `jk`" })
+
+-- save file
+keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+-- Move to window using the <ctrl> hjkl keys
+keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+-- Resize window using <ctrl> arrow keys
+keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
