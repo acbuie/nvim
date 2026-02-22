de1b06970f72e41043b08721654a1a0968fdfa5b
acbuie <aidancbuie@gmail.com>
2026-02-21 20:45:59 -0700

    feat: Tweak floating window bg

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,3 +1,11 @@
 return {
-  "ellisonleao/gruvbox.nvim", config = function() vim.cmd.colorscheme("gruvbox") end
+  "ellisonleao/gruvbox.nvim",
+  config = function()
+    require("gruvbox").setup({
+      overrides = {
+        NormalFloat = { link = "Normal" },
+      },
+    })
+    vim.cmd.colorscheme("gruvbox")
+  end,
 }
