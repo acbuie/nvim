c70c9bede2f0d4ba7b9875e2c41128fa86b63b95
acbuie <aidancbuie@gmail.com>
2026-02-21 20:48:28 -0700

    feat: Add which-key preset

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,9 +1,12 @@
 return {
   "folke/which-key.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons", config = true },
   event = "VeryLazy",
-  init = function() 
+  init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 500
   end,
-  config = true,
+  opts = {
+    preset = "modern",
+  },
 }
