19fed2d5433ab10369f11b8430a0587454f02378
acbuie <aidancbuie@gmail.com>
2026-09-21 12:46:48 -0600

    feat: setup vim.lsp and Mason

---
+++ lsp/lua_ls.lua
@@ -0,0 +1,18 @@
+return {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = {"vim"}
+      },
+      runtime = {
+        version = "LuaJIT",
+      },
+      workspace = {
+        preloadFileSize = 10000,
+        library = {
+          vim.env.VIMRUNTIME,
+        }
+      },
+    },
+  },
+}
---
+++ plugin/00_config/02_lsp.lua
@@ -0,0 +1,9 @@
+vim.pack.add({
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/mason-org/mason.nvim" }
+})
+
+require("mason").setup()
+
+-- Enable
+vim.lsp.enable("lua_ls") -- lua-language-server
