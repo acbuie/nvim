ba997ccd44a812484286a9801718f3d9b6a81d43
acbuie <aidancbuie@gmail.com>
2026-09-23 09:46:15 -0600

    feat: tweak completion menu

--- plugin/10_plugins/10_completion.lua
+++ plugin/10_plugins/10_completion.lua
@@ -1,8 +1,67 @@
 vim.pack.add({
   { src = "https://github.com/saghen/blink.lib" },
   { src = "https://github.com/saghen/blink.cmp", version = "main" },
+  { src = "https://github.com/onsails/lspkind.nvim" },
 })
 
 local cmp = require("blink.cmp")
 cmp.build():pwait()
-cmp.setup()
+
+cmp.setup({
+  appearance = {
+    nerd_font_variant = "normal",
+  },
+  completion = {
+    menu = {
+      draw = {
+        components = {
+          kind_icon = {
+            text = function(ctx)
+              local icon = ctx.kind_icon
+              if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                if dev_icon then
+                  icon = dev_icon
+                end
+              else
+                icon = require("lspkind").symbol_map[ctx.kind] or ""
+              end
+
+              return icon .. ctx.icon_gap
+            end,
+
+            -- Optionally, use the highlight groups from nvim-web-devicons
+            -- You can also add the same function for `kind.highlight` if you want to
+            -- keep the highlight groups in sync with the icons.
+            highlight = function(ctx)
+              local hl = ctx.kind_hl
+              if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                if dev_icon then
+                  hl = dev_hl
+                end
+              end
+              return hl
+            end,
+          },
+        },
+
+        gap = 2,
+        columns = {
+          { "kind_icon" },
+
+          --selene: allow(mixed_table)
+          { "label", "label_description", gap = 1 },
+
+          { "kind" },
+
+          --selene: allow(mixed_table)
+          { "source_name" },
+        },
+      },
+    },
+  },
+  sources = {
+    default = { "lsp", "path", "snippets", "buffer" },
+  },
+})
